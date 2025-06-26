1f14f0852374a46a44c2d00ac695eb71fd5b2f31
Anshulkhambe <151910504+Anshulkhambe@users.noreply.github.com>
2025-06-26 18:40:39 +0530

    Update vendorsiteregistration.component.ts

--- AtoZStore_Frontend/src/app/VendorSiteRegistration/vendorsiteregistration.component.ts
+++ AtoZStore_Frontend/src/app/VendorSiteRegistration/vendorsiteregistration.component.ts
@@ -11,7 +11,7 @@ import Swal from 'sweetalert2';
 })
 export class VendorsiteregistrationComponent {
 
-  private baseUrl:any = "http://localhost:8081";
+  private baseUrl:any = "https://atozstore-ecommerce-website-backend.onrender.com";
 
   isSubmited:boolean=false;
 
