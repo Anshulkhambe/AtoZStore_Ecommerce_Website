f0ac21fee45a292437740c3cfb097b1832d9535a
Anshulkhambe <151910504+Anshulkhambe@users.noreply.github.com>
2025-06-26 18:53:55 +0530

    Update updatevendorprofile.component.ts

--- AtoZStore_Frontend/src/app/VendorHomepage/updatevendorprofile/updatevendorprofile.component.ts
+++ AtoZStore_Frontend/src/app/VendorHomepage/updatevendorprofile/updatevendorprofile.component.ts
@@ -59,7 +59,7 @@ export class UpdatevendorprofileComponent {
 
 
          saveUserInfo() {
-          this.http.post("http://localhost:8081/vendor/update",this.vendor).subscribe(
+          this.http.post("https://atozstore-ecommerce-website-backend.onrender.com/vendor/update",this.vendor).subscribe(
            (response:any)=>{
              console.log(response);
 
