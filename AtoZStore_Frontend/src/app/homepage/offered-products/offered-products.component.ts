36467e522062444588729da082810bd794e6d661
Anshulkhambe <151910504+Anshulkhambe@users.noreply.github.com>
2025-06-26 19:00:53 +0530

    Update offered-products.component.ts

--- AtoZStore_Frontend/src/app/homepage/offered-products/offered-products.component.ts
+++ AtoZStore_Frontend/src/app/homepage/offered-products/offered-products.component.ts
@@ -32,7 +32,7 @@ export class OfferedProductsComponent {
 
 
 
-   baseurl="http://localhost:8081"
+   baseurl="https://atozstore-ecommerce-website-backend.onrender.com"
    constructor(private http:HttpClient,private router:Router,private route: ActivatedRoute,public dialog: MatDialog){
 this.getOfferedProducts()
 
