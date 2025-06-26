3726a70552617a46540bc1beff046d107f9afe27
Anshulkhambe <151910504+Anshulkhambe@users.noreply.github.com>
2025-06-26 18:50:26 +0530

    Update orderhistory.component.ts

--- AtoZStore_Frontend/src/app/Order_History/orderhistory.component.ts
+++ AtoZStore_Frontend/src/app/Order_History/orderhistory.component.ts
@@ -14,7 +14,7 @@ export class OrderhistoryComponent implements OnInit {
   products:any[]=[];
   totalPrice:any="";
 
-  private baseUrl: any = "http://localhost:8081";
+  private baseUrl: any = "https://atozstore-ecommerce-website-backend.onrender.com";
 
   constructor(private http: HttpClient,private router: Router) { }
 
